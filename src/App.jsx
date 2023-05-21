2bdbdcad85fb520d6deb158a6976664d1430d9b8
dinbusan <deann.videtto@gmail.com>
2023-05-21 13:33:55 +0200

    updated the navbar section to be mobile friendly

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
-import { useState } from "react";
 import Navbar from "./components/Navbar";
 import Body from "./components/Body";
 import Footer from "./components/Footer";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
   <div>
 <Navbar />
-<Body />
+<div className="container">
+  <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/about" element={<About />} />
+    <Route path="/contact" element={<Contact />} />
+  </Routes>
+</div>
 <Footer />
   </div>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,72 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { Link, useMatch, useResolvedPath, NavLink } from 'react-router-dom'
+import { FaBars, FaTimes, } from "react-icons/fa";
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  const handleClick = () => setNav(!nav);
   return (
-    <div>Navbar</div>
+    <nav className="bg-gray-800 text-white flex justify-between items-stretch gap-8 p-4">
+      <Link to="/">Salt and Spoon</Link>
+      <ul className="hidden md:flex gap-1">
+        <CustomLink className="h-100 hover:bg-gray-300" to="/">
+          Home
+        </CustomLink>
+        <CustomLink className="h-100 hover:bg-gray-300" to="/about">
+          About
+        </CustomLink>
+        <CustomLink className="hover:bg-gray-300" to="/contact">
+          Contact
+        </CustomLink>
+      </ul>
+      {/*hamburger*/}
+      <div
+        onClick={handleClick}
+        className="md:hidden z-10"
+        style={{ fontSize: "2rem" }}
+      >
+        {!nav ? <FaBars /> : <FaTimes />}
+      </div>
+      {/*mobile menu*/}
+      <ul
+        className={
+          !nav
+            ? "hidden"
+            : "fixed top-0 left-0 w-full h-screen bg-black object-cover roddenberry flex flex-col justify-center items-center"
+        }
+      >
+        <li className="py-6 text-4xl">
+          <NavLink onClick={handleClick} to="/">
+            Home
+          </NavLink>
+        </li>
+        <li className="py-6 text-4xl">
+          {" "}
+          <NavLink onClick={handleClick} to="/about">
+            About
+          </NavLink>
+        </li>
+        <li className="py-6 text-4xl">
+          {" "}
+          <NavLink onClick={handleClick} to="/contact">
+            Contact
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+function CustomLink({to, children, ...props}) {
+  const resolvedPath = useResolvedPath(to);
+  const isActive = useMatch({path: resolvedPath.pathname, end: true});
+
+  return (
+    <li className={isActive ? "active" : ""}>
+      <Link to={to} {...props}>
+        {children}
+      </Link>
+    </li>
   )
 }
 
