d15b65b485548e348763417f1bfbba2a365a30d6
dinbusan <deann.videtto@gmail.com>
2023-05-24 23:50:47 +0200

    updated parallax scroll on home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,32 +3,38 @@ import { motion, useScroll, useTransform } from "framer-motion";
 import Logo from "../assets/invert.png";
 
 const Home = () => {
-  
+  let { scrollYProgress } = useScroll();
+  let y = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
+
   return (
-    <>
-      <div className="bg-cupcakes w-screen bg-fixed bg-no-repeat bg-cover bg-center">
-        <div className="img-container  w-3/4 sm:w-2/3 md:w-2/5 lg:w-2/3 xl:w-2/5 xl:py-5 mx-auto">
-          <img className="logo" src={Logo} alt="" />
+    <div className="relative">
+      <motion.div className=" relative" style={{ y }}>
+        <div className="bg-cupcakes w-screen bg-fixed bg-no-repeat bg-cover bg-center">
+          <div className="img-container w-3/4 sm:w-2/3 md:w-2/5 lg:w-2/3 xl:w-2/5 xl:py-5 mx-auto">
+            <img className="logo" src={Logo} alt="" />
+          </div>
         </div>
-      </div>
-      <div className="w-5/6 my-5 md:my-10 mx-auto grid grid-cols-1 md:grid-cols-5 bg-white">
-        <div className="md:col-span-3 py-5 md:pt-10 xl:pt-20">
-          <h1 className="font-shadows text-4xl xl:text-6xl text-center justify-center flex py-5 md:py-10">
-            Welcome to <br className="lg:hidden" /> Salt & Spoon!
-          </h1>
-          <p className="font-thasadith text-xl  px-2 pb-2 xl:px-24 text-center flex ">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam,
-            voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Facilis, optio. Praesentium amet excepturi officia quis atque
-            sapiente ea animi nulla?
-          </p>
-        </div>
-        <div className="welcome-image md:col-span-2 flex">
-          <img
-            src="src/assets/welcome-img.jpeg"
-            alt=""
-            className="object-cover"
-          />
+      </motion.div>
+      <div className="z-1 relative w-screen bg-blue-50">
+        <div className=" w-5/6 my-5 md:my-10 mx-auto grid grid-cols-1 md:grid-cols-5 bg-white">
+          <div className="md:col-span-3 py-5 md:pt-10 xl:pt-20">
+            <h1 className="font-shadows text-4xl xl:text-6xl text-center justify-center flex py-5 md:py-10">
+              Welcome to <br className="lg:hidden" /> Salt & Spoon!
+            </h1>
+            <p className="font-thasadith text-xl  px-2 pb-2 xl:px-24 text-center flex ">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam,
+              voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing
+              elit. Facilis, optio. Praesentium amet excepturi officia quis
+              atque sapiente ea animi nulla?
+            </p>
+          </div>
+          <div className="welcome-image md:col-span-2 flex">
+            <img
+              src="src/assets/welcome-img.jpeg"
+              alt=""
+              className="object-cover"
+            />
+          </div>
         </div>
       </div>
       <div className="img-container gap-1 px-2 md:px-0 w-screen grid grid-cols-2 md:grid-cols-5 md:gap-0">
@@ -103,7 +109,7 @@ const Home = () => {
           />
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
