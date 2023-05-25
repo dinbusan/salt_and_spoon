c50c424c7e4b162fe3d6db5f8647a148f7839307
dinbusan <deann.videtto@gmail.com>
2023-05-25 14:40:34 +0200

    updated the parallax scrolling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,16 +4,16 @@ import Logo from "../assets/invert.png";
 import Welcome from "../assets/welcome-img.jpeg";
 
 const Home = () => {
-  let ref = useRef(null);
-  let { scrollYProgress } = useScroll({
-    target: ref,
-    offset: ["start start", "end start"]
-  });
-  let y = useTransform(scrollYProgress, [0, 1], ["0%", "50%"]);
+  // let ref = useRef(null);
+  // let { scrollYProgress } = useScroll({
+  //   target: ref,
+  //   offset: ["start start", "end start"]
+  // });
+  // let y = useTransform(scrollYProgress, [0, 1], ["0%", "50%"]);
 
   return (
-    <div  className="relative">
-      <div ref={ref}>
+    <div className="relative">
+      {/* <div ref={ref}>
         <motion.div className=" relative" style={{ y }}>
           <div className="bg-bluecake w-screen bg-fixed bg-no-repeat bg-cover bg-center">
             <div className="img-container w-3/4 sm:w-2/3 md:w-2/5 lg:w-2/3 xl:w-2/5 py-2 xl:py-5 mx-auto">
@@ -21,7 +21,8 @@ const Home = () => {
             </div>
           </div>
         </motion.div>
-      </div>
+      </div> */}
+      <Section />
       <div className="z-1 relative w-screen bg-blue-50">
         <div className=" w-5/6 my-5 md:my-10 mx-auto grid grid-cols-1 md:grid-cols-5 bg-white">
           <div className="md:col-span-3 py-5 md:pt-10 xl:pt-20">
@@ -36,88 +37,108 @@ const Home = () => {
             </p>
           </div>
           <div className="welcome-image md:col-span-2 flex">
-            <img
-              src={Welcome}
-              alt=""
-              className="object-cover"
-            />
+            <img src={Welcome} alt="" className="object-cover" />
           </div>
         </div>
       </div>
-      <div className="img-container gap-1 px-2 md:px-0 w-screen grid grid-cols-2 md:grid-cols-5 md:gap-0">
-        <div className="aspect-square card-zoom">
-          <img
-            src="src/assets/img1.jpeg"
-            alt=""
-            className="card-zoom-image object-cover object-center w-full h-full "
-          />
-        </div>
-        <div className="aspect-square card-zoom">
-          <img
-            src="src/assets/img2.jpeg"
-            alt=""
-            className="card-zoom-image object-cover object-center w-full h-full"
-          />
-        </div>
-        <div className="aspect-square card-zoom">
-          <img
-            src="src/assets/img3.jpeg"
-            alt=""
-            className="card-zoom-image object-cover object-center w-full h-full"
-          />
-        </div>
-        <div className="aspect-square card-zoom">
-          <img
-            src="src/assets/img4.jpeg"
-            alt=""
-            className="card-zoom-image object-cover object-center w-full h-full"
-          />
-        </div>
-        <div className="aspect-square card-zoom">
-          <img
-            src="src/assets/img5.jpeg"
-            alt=""
-            className="card-zoom-image object-cover object-center w-full h-full"
-          />
-        </div>
-        <div className="aspect-square card-zoom">
-          <img
-            src="src/assets/img6.jpeg"
-            alt=""
-            className="card-zoom-image object-cover object-center w-full h-full"
-          />
-        </div>
-        <div className="aspect-square card-zoom">
-          <img
-            src="src/assets/img7.jpeg"
-            alt=""
-            className="card-zoom-image object-cover object-center w-full h-full"
-          />
-        </div>
-        <div className="aspect-square card-zoom">
-          <img
-            src="src/assets/img8.jpeg"
-            alt=""
-            className="card-zoom-image object-cover object-center w-full h-full"
-          />
-        </div>
-        <div className="aspect-square card-zoom">
-          <img
-            src="src/assets/img9.jpeg"
-            alt=""
-            className="card-zoom-image object-cover object-center w-full h-full"
-          />
-        </div>
-        <div className="aspect-square card-zoom">
-          <img
-            src="src/assets/img10.jpeg"
-            alt=""
-            className="card-zoom-image object-cover object-center w-full h-full"
-          />
+      <Section />
+      <div className="">
+        <div className="bg-sky-50 mt-10 img-container gap-1 px-2 md:px-0 w-screen grid grid-cols-2 md:grid-cols-5 md:gap-0">
+          <div className="aspect-square card-zoom">
+            <img
+              src="src/assets/img1.jpeg"
+              alt=""
+              className="card-zoom-image object-cover object-center w-full h-full "
+            />
+          </div>
+          <div className="aspect-square card-zoom">
+            <img
+              src="src/assets/img2.jpeg"
+              alt=""
+              className="card-zoom-image object-cover object-center w-full h-full"
+            />
+          </div>
+          <div className="aspect-square card-zoom">
+            <img
+              src="src/assets/img3.jpeg"
+              alt=""
+              className="card-zoom-image object-cover object-center w-full h-full"
+            />
+          </div>
+          <div className="aspect-square card-zoom">
+            <img
+              src="src/assets/img4.jpeg"
+              alt=""
+              className="card-zoom-image object-cover object-center w-full h-full"
+            />
+          </div>
+          <div className="aspect-square card-zoom">
+            <img
+              src="src/assets/img5.jpeg"
+              alt=""
+              className="card-zoom-image object-cover object-center w-full h-full"
+            />
+          </div>
+          <div className="aspect-square card-zoom">
+            <img
+              src="src/assets/img6.jpeg"
+              alt=""
+              className="card-zoom-image object-cover object-center w-full h-full"
+            />
+          </div>
+          <div className="aspect-square card-zoom">
+            <img
+              src="src/assets/img7.jpeg"
+              alt=""
+              className="card-zoom-image object-cover object-center w-full h-full"
+            />
+          </div>
+          <div className="aspect-square card-zoom">
+            <img
+              src="src/assets/img8.jpeg"
+              alt=""
+              className="card-zoom-image object-cover object-center w-full h-full"
+            />
+          </div>
+          <div className="aspect-square card-zoom">
+            <img
+              src="src/assets/img9.jpeg"
+              alt=""
+              className="card-zoom-image object-cover object-center w-full h-full"
+            />
+          </div>
+          <div className="aspect-square card-zoom">
+            <img
+              src="src/assets/img10.jpeg"
+              alt=""
+              className="card-zoom-image object-cover object-center w-full h-full"
+            />
+          </div>
         </div>
       </div>
     </div>
   );
 };
+
+function Section() {
+  let ref = useRef(null);
+  let {scrollYProgress} = useScroll({
+    target: ref, 
+    offset: ["start start", "end start"],
+  });
+  let y = useTransform(scrollYProgress, [0, 1], ["0%", "50%"]);
+
+  return (
+    <div ref={ref}>
+      <motion.div className="" style={{ y }}>
+        <div className="bg-bluecake w-screen bg-fixed bg-no-repeat bg-cover bg-center">
+          <div className="img-container w-3/4 sm:w-2/3 md:w-2/5 lg:w-2/3 xl:w-2/5 py-2 xl:py-5 mx-auto">
+            <img className="logo" src={Logo} alt="" />
+          </div>
+        </div>
+      </motion.div>
+    </div>
+  );
+}
 
 export default Home;
