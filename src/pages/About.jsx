54dd9ebdb9e88ea406e7c1da34950c77ccbaf631
dinbusan <deann.videtto@gmail.com>
2023-05-29 14:41:16 +0200

    updating padding on home page welcome message, updated about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
-import Welcome from "../assets/welcome-img.jpeg"
+import Welcome from "../assets/welcome-img.jpeg";
 
 const About = () => {
   return (
@@ -11,8 +11,8 @@ const About = () => {
       transition={{ duration: 0.5 }}
       className="pb-5"
     >
-      <div className="bg-sky-950 w-full text-blue-50 text-center font-thasadith tracking-wider text-3xl py-16 -mt-1">
-        OUR STORY
+      <div className="bg-sky-950 w-full text-blue-50 text-center font-thasadith tracking-wider text-3xl md:text-4xl xl:text-5xl py-16 -mt-1">
+        MY STORY
       </div>
       <div className="md:grid md:grid-cols-2 xl:grid-cols-4 md:my-10 md:mr-5 bg-gray-50">
         <img
@@ -20,19 +20,11 @@ const About = () => {
           src={Welcome}
           alt=""
         />
-        <div className="mx-5 my-auto xl:col-start-2">
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas,
-            neque ad. Doloremque voluptatibus recusandae magnam sapiente sed,
-            velit ratione, iste ipsum, maxime perspiciatis adipisci voluptas!
-            Iusto accusantium, ullam laboriosam quibusdam nobis sapiente quo.
-            Ratione eius quod minima quis ut, vitae, quas inventore iste velit
-            nam enim illum eveniet laborum. Placeat optio architecto nemo
-            voluptatum consequuntur eum quae quasi corrupti ut nisi! Excepturi
-            laudantium iste dignissimos vitae molestiae ab ut, harum quod sed
-            quo quam, quis incidunt atque unde, sapiente culpa assumenda dolore
-            nemo? Hic ipsam facere, cupiditate dicta, dolor a ea non, et placeat
-            molestiae quae sed officiis sapiente aut!
+        <div className="mx-5 mt-10 md:mt-20 xl:col-start-2 font-thasadith">
+          <p className="flex flex-col tracking-wider text-center">
+            <span className="text-center  tracking-widest font-bold">MEET GAIL</span>
+            <hr class="w-10 h-0.5 mx-auto mt-4 mb-7 bg-gray-900 border-0" />
+            Salt and Spoon is run as a one woman show, by Gail Gianelli.
           </p>
         </div>
       </div>
