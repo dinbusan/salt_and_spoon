6266a96d325b6c1d04a37502bff35e2553b82976
dinbusan <deann.videtto@gmail.com>
2023-07-01 00:12:31 +0200

    updated the cakes page

--- src/pages/Cakes.jsx
+++ src/pages/Cakes.jsx
@@ -14,51 +14,122 @@ const Cakes = () => {
         <div className="bg-sky-950 w-full text-blue-50 text-center font-thasadith tracking-wider text-3xl md:text-4xl xl:text-5xl py-16 -mt-1">
           CAKES
         </div>
-        <div>
-          Frosting- cream cheese and buttercream
-          <br />
-          Fillings- buttercream, cream cheese, mousse and fruit available
-          <br />
-          6” cake- $25
-          <br />
-          8” cake- serves up to 20 - $45
-          <br />
-          9” cake- serves up to 24- $ 50
-          <br />
-          10” cake- serves up to 28- $ 55
-          <br />
-          Tiered cakes and sheet cake also available
-          <br />
-          Specialty designs - added cost accordingly
-        </div>
-        <div>
-          <h1>Flavors</h1>
-          PBDC Our Rock Star Our signature dark chocolate cake with peanut
-          butter, cream filling & buttercream frosting Lemon Razzy White vanilla
-          cake, lemon curd filling & raspberry buttercream frosting. Light with
-          a pop of razz! Mom’s Tiramisu Coffee-flavored white cake with
-          mascarpone coffee filling & buttercream frosting Toasted Almond Crunch
-          White vanilla cake with almond buttercream filling & vanilla crunch
-          topping. It’ll take you back to the ice cream truck! Midnight Mint
-          Chip Rich dark chocolate chip cake with mint mousse filling, topped
-          with buttercream frosting. Fresh and decadent. Cookies and Cream
-          Chocolate cake with cookie crumble filling & vanilla buttercream
-          frosting.Gimme Smores Our signature dark chocolate cake with a layer
-          of graham cracker crumbs, chocolate bar filling & toasted meringue
-          frosting. No camp fires needed! Blueberry Bliss White cake with fresh
-          blueberries, mascarpone filling & buttercream frosting Not your
-          Mother’s Carrot Cake Spiced cake with coconut, raisins, pineapple &
-          cream cheese topped with buttercream frosting Coffee Crunch Dark
-          chocolate cake with decadent chocolate fudge, dipped in toffee pieces
-          with fresh whipped cream Strawberries & Champagne White champagne
-          laced cake with strawberry cream filling & whipped cream topping.
-          Perfect for any occasion! Blackberry Dream Chocolate cake with
-          mascarpone filling, blackberry buttercream frosting & fresh
-          blackberries Nonna’s Cannoli White cake with mascarpone & cream cheese
-          filling with fresh whipped cream frosting Birthday Bombs White cake
-          with colored shots & vanilla buttercream frosting. Perfect for kids &
-          adults! Breakfast for Dessert Warm spice cake with bacon, maple
-          filling & maple butter- cream frosting topped with candied bacon.
+        <div className="list text-center mx-10">
+          <div className="sizes">
+            <h1 className="font-bold text-lg underline">Sizes</h1>
+            <ul>
+              <li>6” cake- $25</li>
+              <li>8” cake- serves up to 20 - $45</li>
+              <li>9” cake- serves up to 24 - $ 50</li>
+              <li>10” cake- serves up to 28 - $ 55</li>
+            </ul>
+          </div>
+          <div>
+            <div className="frosting">
+              <h1 className="font-bold text-lg underline">Frosting</h1>
+              <ul>
+                <li>cream cheese</li>
+                <li>buttercream</li>
+              </ul>
+            </div>
+            <div className="fillings">
+              <h1 className="font-bold text-lg underline">Fillings</h1>
+              <ul>
+                <li>buttercream</li>
+                <li>cream cheese</li>
+                <li>mousse</li>
+                <li>fruit</li>
+              </ul>
+            </div>
+            Tiered cakes and sheet cake also available
+            <br />
+            Specialty designs - added cost accordingly
+          </div>
+          <div>
+            <div className="flavors">
+              <h1 className="font-bold text-lg underline">Flavors</h1>
+              <ul>
+                <li>
+                  <span className="font-bold">PBDC (Our Rock Star) -</span> Our
+                  signature dark chocolate cake with peanut butter, cream
+                  filling & buttercream frosting
+                </li>
+                <li>
+                  <span className="font-bold">Lemon Razzy -</span> White vanilla
+                  cake, lemon curd filling & raspberry buttercream frosting.
+                  Light with a pop of razz!
+                </li>
+                <li>
+                  <span className="font-bold">Mom’s Tiramisu Coffee -</span>{" "}
+                  flavored white cake with mascarpone coffee filling &
+                  buttercream frosting
+                </li>
+                <li>
+                  <span className="font-bold">Toasted Almond Crunch -</span>{" "}
+                  White vanilla cake with almond buttercream filling & vanilla
+                  crunch topping. It’ll take you back to the ice cream truck!
+                </li>
+                <li>
+                  <span className="font-bold">Midnight Mint Chip -</span> Rich
+                  dark chocolate chip cake with mint mousse filling, topped with
+                  buttercream frosting. Fresh and decadent.
+                </li>
+                <li>
+                  <span className="font-bold">Cookies and Cream -</span>{" "}
+                  Chocolate cake with cookie crumble filling & vanilla
+                  buttercream frosting
+                </li>
+                <li>
+                  <span className="font-bold">Gimme Smores -</span> Our
+                  signature dark chocolate cake with a layer of graham cracker
+                  crumbs, chocolate bar filling & toasted meringue frosting. No
+                  camp fires needed!{" "}
+                </li>
+                <li>
+                  <span className="font-bold">Blueberry Bliss -</span> White
+                  cake with fresh blueberries, mascarpone filling & buttercream
+                  frosting{" "}
+                </li>
+                <li>
+                  <span className="font-bold">
+                    Not your Mother’s Carrot Cake -
+                  </span>{" "}
+                  Spiced cake with coconut, raisins, pineapple & cream cheese
+                  topped with buttercream frosting{" "}
+                </li>
+                <li>
+                  <span className="font-bold">Coffee Crunch -</span> Dark
+                  chocolate cake with decadent chocolate fudge, dipped in toffee
+                  pieces with fresh whipped cream{" "}
+                </li>
+                <li>
+                  <span className="font-bold">Strawberries & Champagne -</span>{" "}
+                  White champagne laced cake with strawberry cream filling &
+                  whipped cream topping. Perfect for any occasion!
+                </li>
+                <li>
+                  <span className="font-bold">Blackberry Dream -</span>{" "}
+                  Chocolate cake with mascarpone filling, blackberry buttercream
+                  frosting & fresh blackberries{" "}
+                </li>
+                <li>
+                  <span className="font-bold">Nonna’s Cannoli -</span> White
+                  cake with mascarpone & cream cheese filling with fresh whipped
+                  cream frosting{" "}
+                </li>
+                <li>
+                  <span className="font-bold">Birthday Bombs -</span> White cake
+                  with colored shots & vanilla buttercream frosting. Perfect for
+                  kids & adults!
+                </li>
+                <li>
+                  <span className="font-bold">Breakfast for Dessert -</span>{" "}
+                  Warm spice cake with bacon, maple filling & maple buttercream
+                  frosting topped with candied bacon.
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
       </motion.div>
     </div>
